92da5916ac300e20c49f4e64eb6a1a5e115fc0cf
Dhiraj Borade <dhirajborade@gmail.com>
2017-09-23 22:28:05 -0400

    Final Commit

--- src/com/Heap/NodeFibHeap.java
+++ src/com/Heap/NodeFibHeap.java
@@ -1,41 +1,41 @@
 package com.Heap;
 
 /**
- * <b>Node Structure:</b>
- * 	Attributes: Key, Data, Degree
- * 	|Pointers: Parent, Child, Left Sibling, Right Sibling
- * 	|Boolean: ChildCut 
- * @author Jagan
+ * <b>Node Structure:</b> Attributes: Key, Data, Degree |Pointers: Parent,
+ * Child, Left Sibling, Right Sibling |Boolean: ChildCut
+ * 
+ * @author Dhiraj
  *
  */
-public class NodeFibHeap{
-	
+public class NodeFibHeap {
+
 	public int degree = 0;
 	public Integer data = null;
 	public String key;
-	
+
 	public NodeFibHeap parent = null;
 	public NodeFibHeap child = null;
 	public NodeFibHeap leftSibling = null;
 	public NodeFibHeap rightSibling = null;
-	
+
 	public boolean childCut = false;
-	
-	public NodeFibHeap(){}
-	
-	public NodeFibHeap(String key,int data){
+
+	public NodeFibHeap() {
+	}
+
+	public NodeFibHeap(String key, int data) {
 		this.key = key;
 		this.data = data;
 	}
-	
-	public NodeFibHeap(NodeFibHeap obj){
+
+	public NodeFibHeap(NodeFibHeap obj) {
 		key = obj.key;
 		data = obj.data;
 		degree = obj.degree;
 		parent = obj.parent;
 		child = obj.child;
-		leftSibling= obj.leftSibling;
+		leftSibling = obj.leftSibling;
 		rightSibling = obj.rightSibling;
-		childCut= obj.childCut;
+		childCut = obj.childCut;
 	}
 }
